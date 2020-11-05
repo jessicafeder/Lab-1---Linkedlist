66aba045af073f2cec9f1a7d53e6e1a9cea140f4
Jessica Feder <jessica.feder@student.kyh.se>
2020-11-05 18:58:59 +0100

    Small updates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,27 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
-        list.add(5);
         list.add(1);
+        list.add(2);
         list.add(3);
-        list.add(6);
-        list.add(5);
 
         System.out.println("LinkedList \n~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Current list: " + list);
-        System.out.println("List size before adding element: " + list.size()); // --> 5
+        System.out.println("List size before adding element: " + list.size()); // --> 3
 
-        list.add(0,2);
+        list.add(0,99);
 
-        System.out.println("List size after adding element: " + list.size()); // --> 6
+        System.out.println("List size after adding element: " + list.size()); // --> 4
 
-        System.out.println("Get index 0: " + list.get(0)); // --> 2
-        System.out.println("Get index 4: " + list.get(4)); // --> 6
+        System.out.println("Get index 0: " + list.get(0)); // --> 99
+        System.out.println("Get index 4: " + list.get(3)); // --> 3
 
-        list.remove(5);
+        list.remove(3);
 
-        System.out.println("List size after removing element: " + list.size()); // --> 5
+        System.out.println("List size after removing element: " + list.size()); // --> 3
         System.out.println("Returns true if list contains element 3: " + list.contains(3)); // --> returns true
+        System.out.println("Returns false as list is not empty: " + list.isEmpty());
 
         Stack <Integer> stack = new Stack<>();
         System.out.println("\nStack\n~~~~~~~~~~~~~~~~~~~~~~~~~");
